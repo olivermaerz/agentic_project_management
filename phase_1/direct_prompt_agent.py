fa875e88e5d9339d9e1c42d704447bf587598948
olivermaerz <github@olivermaerz.com>
2026-08-01 22:50:27 +0200

    Update direct_prompt_agent test

--- phase_1/direct_prompt_agent.py
+++ phase_1/direct_prompt_agent.py
@@ -7,17 +7,22 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# TODO: 2 - Load the OpenAI API key from the environment variables
+# Load the OpenAI API key from the environment variables from the .env file
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+# Define the prompt for the agent
 prompt = "What is the Capital of France?"
 
-# TODO: 3 - Instantiate the DirectPromptAgent as direct_agent
-# TODO: 4 - Use direct_agent to send the prompt defined above and store the response
-direct_agent_response = DirectPromptAgent(openai_api_key).respond(prompt)
+# Instantiation of the DirectPromptAgent as direct_agent
+direct_agent = DirectPromptAgent(openai_api_key)
+# Using direct_agent to send the prompt defined above and store the response
+direct_agent_response = direct_agent.respond(prompt)
 
 # Print the response from the agent
+print("\nResponse from the agent:")
+print("-"*100)
 print(direct_agent_response)
+print("-"*100)
 
-# TODO: 5 - Print an explanatory message describing the knowledge source used by the agent to generate the response
-print()
+# Explanatory message describing the knowledge source used by the agent to generate the response
+print(f"The agent used the following knowledge source LLM model gpt-3.5-turbo from OpenAI to generate the response.\n")
